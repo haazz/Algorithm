0e1291e0359e4ecab38cd07597ca6b0bd662f005
haazz <127824457+haazz@users.noreply.github.com>
2024-08-21 13:30:08 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 320 ms, Memory: 57,720 KB -BaekjoonHub

---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/README.md
@@ -0,0 +1,15 @@
+# [D5] 현주가 좋아하는 제곱근 놀이 - 6782 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWgqsAlKr9sDFAW0) 
+
+### 성능 요약
+
+메모리: 57,720 KB, 시간: 320 ms, 코드길이: 1,015 Bytes
+
+### 제출 일자
+
+2024-08-21 13:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Solution {
+
+    public static long dfs(long n, long cnt) {
+        // System.out.println(n + " " + cnt);
+        if (n < 2) {
+            return -1;
+        }
+        if (n == 2) {
+            return cnt;
+        }
+
+        double sqrtNum = Math.sqrt(n);
+
+        if (sqrtNum % 1 == 0) {
+            return dfs((long) sqrtNum, cnt + 1);
+        }
+        // 다음 제곱근 가능한 수로 변경
+        long nNum = (long) (sqrtNum + 1) * (long) (sqrtNum + 1);
+        // 증가한 만큼 cnt
+        //
+        return dfs(nNum, cnt + nNum - n);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            long n = sc.nextLong();
+            // System.out.println("***********");
+            System.out.println("#" + tc + " " + dfs(n, 0));
+        }
+    }
+}
