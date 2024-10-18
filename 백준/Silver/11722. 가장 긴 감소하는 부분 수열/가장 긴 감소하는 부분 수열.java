1f10cb6ec3e6d53409e5563d52dabe03d044fe43
haazz <127824457+haazz@users.noreply.github.com>
2024-10-18 10:11:27 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+public class Main {
+    static int N;
+    static int idx;
+
+    public static int binarySearch(int[] array, int from, int to, int key) {
+        int result = 0;
+
+        while (from <= to) {
+            int mid = (from + to) / 2;
+
+            if (key >= array[mid]) {
+                to = mid - 1;
+                result = mid;
+
+            } else {
+                from = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        int[] sequence = new int[N];
+        int[] dp = new int[N];
+        idx = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MIN_VALUE;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int currentIdx = binarySearch(dp, 0, idx + 1, sequence[i]);
+
+            if (currentIdx > idx) {
+                idx++;
+            }
+            dp[currentIdx] = sequence[i];
+        }
+
+        System.out.println(idx + 1);
+    }
+}
