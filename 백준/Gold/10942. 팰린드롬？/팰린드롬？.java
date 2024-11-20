a38b572c0cff6c99d900b1f0c6eecc62adfc7f96
haazz <127824457+haazz@users.noreply.github.com>
2024-11-20 22:53:37 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1260 ms, Memory: 214972 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] nums;
+
+    public static int pelin(int s, int e) {
+        int diff = (e - s + 1) / 2;
+        for (int j = 0; j < diff; j++) {
+            if (nums[s + j] != nums[e - j]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            sb.append(pelin(s, e));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+
+    }
+}
