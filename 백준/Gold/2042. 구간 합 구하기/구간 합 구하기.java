6bc726a611f5319023c822d7658fdfc8fe3d235f
haazz <127824457+haazz@users.noreply.github.com>
2024-12-11 01:36:15 +0900

    [Gold I] Title: 구간 합 구하기, Time: 540 ms, Memory: 117460 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, K;
+    static long[] nums;
+    static long[] tree;
+
+    public static long initTree(int start, int end, int node) {
+        if (start == end) {
+            return tree[node] = nums[start];
+        }
+
+        int mid = (start + end) / 2;
+        return tree[node] = initTree(start, mid, node * 2) + initTree(mid + 1, end, node * 2 + 1);
+    }
+
+    public static long sum(int start, int end, int node, int left, int right) {
+        if (left > end || right < start) {
+            return 0;
+        }
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+    }
+
+    public static void update(int start, int end, int node, int idx, long dif) {
+        if (idx < start || idx > end) {
+            return;
+        }
+
+        tree[node] += dif;
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        update(start, mid, node * 2, idx, dif);
+        update(mid + 1, end, node * 2 + 1, idx, dif);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        nums = new long[N + 1];
+        tree = new long[4 * N];
+
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Long.parseLong(br.readLine());
+        }
+        initTree(1, N, 1);
+
+        for (int i = 0; i < M + K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+
+            if (a == 1) {
+                long diff = c - nums[b];
+                nums[b] = c;
+                update(1, N, 1, b, diff);
+            } else {
+                sb.append(sum(1, N, 1, b, (int) c)).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
