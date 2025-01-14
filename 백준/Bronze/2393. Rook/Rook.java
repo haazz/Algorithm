f7a4c4a6bf73a72376a5ce369811b7f7b2dad18a
haazz <127824457+haazz@users.noreply.github.com>
2025-01-15 00:55:01 +0900

    [Bronze V] Title: Rook, Time: 60 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("  ___  ___  ___\n" + //
+                "  | |__| |__| |\n" + //
+                "  |           |\n" + //
+                "   \\_________/\n" + //
+                "    \\_______/\n" + //
+                "     |     |\n" + //
+                "     |     |\n" + //
+                "     |     |\n" + //
+                "     |     |\n" + //
+                "     |_____|\n" + //
+                "  __/       \\__\n" + //
+                " /             \\\n" + //
+                "/_______________\\ ");
+    }
+}
