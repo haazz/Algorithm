82d978522256daa0ac936aac1e66ba86beea5bb1
haazz <127824457+haazz@users.noreply.github.com>
2025-07-04 21:54:58 +0900

    Time: 646 ms (5.03%), Space: 55 MB (41.76%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,44 @@
+/*
+누적합으로 간단하게 풀 수 있을듯?
+for문 두 번만 돌면됨
+
+-> 아 이거 이분탐색 문제다.
+1. 다 더한 값의 절반이 될 수 있는 subset을 찾는다. (이분탐색으로)
+2. 찾으면 true
+3. 아니면 flase
+
+-> 이분 탐색이 안되네
+1. combination 구하기
+
+-> 이분 탐색 되지 않나?
+
+-> dp로 도달 가능한 수를 모두 체크
+-> 최대 20000번만 돌면 됨
+*/
+class Solution {
+
+    public boolean canPartition(int[] nums) {
+        int target = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            target += nums[i];
+        }
+        if (target % 2 != 0) {
+            return false;
+        }
+        target /= 2;
+
+        Set<Integer> combs = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> tmp = new HashSet<>();
+            tmp.add(nums[i]);
+            for (int elem : combs) {
+                tmp.add(elem + nums[i]);
+            }
+            combs.addAll(tmp);
+        }
+
+        return combs.contains(target);
+    }
+}
