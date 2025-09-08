019ca359402a22178061189544080b7502941eb3
haazz <127824457+haazz@users.noreply.github.com>
2025-09-08 12:03:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51352 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,38 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int N;
-    static int K;
-
+class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         int[][] things = new int[N][2];
-        int[][] dp = new int[N + 1][K + 1];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             things[i][0] = Integer.parseInt(st.nextToken());
             things[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int n = 1; n <= N; n++) {
-            for (int k = 1; k <= K; k++) {
-                if (things[n - 1][0] > k) {
-                    dp[n][k] = dp[n - 1][k];
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= K; j++) {
+                int prev = j - things[i][0];
+                if (prev < 0) {
+                    dp[i + 1][j] = dp[i][j];
                     continue;
                 }
-                dp[n][k] = Math.max(dp[n - 1][k], dp[n - 1][k - things[n - 1][0]] + things[n - 1][1]);
+                dp[i + 1][j] = Math.max(dp[i][j], dp[i][prev] + things[i][1]);
             }
         }
-
+        // for (int i = 0; i < N + 1; i++) {
+        // for (int j = 0; j < K + 1; j++) {
+        // System.out.print(dp[i][j] + " ");
+        // }
+        // System.out.println("");
+        // }
         System.out.println(dp[N][K]);
-
     }
 }
