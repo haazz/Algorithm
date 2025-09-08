d32f7dd2a66c7cd53b78e94fb3837d4c6b85d2be
haazz <127824457+haazz@users.noreply.github.com>
2025-09-08 11:17:15 +0900

    Time: 13 ms (90.67%), Space: 44.1 MB (96.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-
-        for (int i = 0; i < coins.length + 1; i++) {
-            for (int j = 0; j < amount + 1; j++) {
-                dp[i][j] = 100000;
-            }
+        int[] dp = new int[amount + 1];
+        for (int i = 0; i < amount + 1; i++) {
+            dp[i] = 100000;
         }
-        dp[0][0] = 0;
+        dp[0] = 0;
 
-        for (int i = 0; i < coins.length; i++) {
-            int k = 0;
-            while (k * coins[i] <= amount) {
-                for (int j = 0; j < amount + 1; j++) {
-                    int next = k * coins[i] + j;
-                    if (next > amount) {
-                        continue;
-                    }
-                    dp[i + 1][next] = Math.min(dp[i + 1][next], Math.min(dp[i][next], dp[i][j] + k));
+        for (int i = 0; i < amount + 1; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                long next = (long)coins[j] + i;
+                // System.out.println(next);
+                if (next > amount) {
+                    continue;
                 }
-                k++;
+                dp[(int)next] = Math.min(dp[i] + 1, dp[(int)next]);
             }
         }
 
-        // for (int i = 0; i < coins.length + 1; i++) {
-        //     for (int j = 0; j < amount + 1; j++) {
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        if (dp[coins.length][amount] == 100000) {
+        if (dp[amount] == 100000) {
             return -1;
         }
-        return dp[coins.length][amount];
+        return dp[amount];
     }
 }
