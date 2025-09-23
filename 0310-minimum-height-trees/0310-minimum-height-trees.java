6c30fea6f7161a7c230fd92280a8863e68b78bbc
haazz <127824457+haazz@users.noreply.github.com>
2025-09-23 11:38:43 +0900

    Time: 28 ms (42.35%), Space: 57.1 MB (27.27%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -4,67 +4,11 @@
 
 class Solution {
     Map<Integer, List<Integer>> tree = new HashMap<>();
-    List<Integer> answer = new ArrayList<>();
-    int maxHeight = 0;
-    int[] heights;
-
-    public int getHeight(int n, int start) {
-        boolean[] visit = new boolean[n];
-        Queue<int[]> q = new LinkedList<>();
-        int maxNode = start;
-        q.add(new int[] {start, 1});
-        visit[start] = true;
-
-        while (!q.isEmpty()) {
-            int[] elem = q.poll();
-
-            for (int nNode : tree.get(elem[0])) {
-                if (visit[nNode]) {
-                    continue;
-                }
-                visit[nNode] = true;
-                q.add(new int[] {nNode, elem[1] + 1});
-                heights[nNode] = Math.max(elem[1] + 1, heights[nNode]);
-                if (maxHeight < elem[1] + 1) {
-                    maxHeight = elem[1] + 1;
-                    maxNode = nNode;
-                }
-            }
-        }
-        return maxNode;
-    }
-
-    public int findStart(int n) {
-        boolean[] visit = new boolean[n];
-        Queue<int[]> q = new LinkedList<>();
-        int maxNode = 0;
-        int height = 0;
-        q.add(new int[] {0, 1});
-        visit[0] = true;
-
-        while (!q.isEmpty()) {
-            int[] elem = q.poll();
-
-            for (int nNode : tree.get(elem[0])) {
-                if (visit[nNode]) {
-                    continue;
-                }
-                visit[nNode] = true;
-                q.add(new int[] {nNode, elem[1] + 1});
-                if (height < elem[1] + 1) {
-                    height = elem[1] + 1;
-                    maxNode = nNode;
-                }
-            }
-        }
-        return maxNode;
-    }
 
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n <= 1) {
-            answer.add(0);
-            return answer;
-        }
+        int remainNode = n;
+        int[] numEdges = new int[n];
+
         for (int i = 0; i < edges.length; i++) {
             if (!tree.containsKey(edges[i][0])) {
                 tree.put(edges[i][0], new ArrayList<>());
@@ -74,20 +18,37 @@ class Solution {
             }
             tree.get(edges[i][0]).add(edges[i][1]);
             tree.get(edges[i][1]).add(edges[i][0]);
+            numEdges[edges[i][0]]++;
+            numEdges[edges[i][1]]++;
         }
 
-        int startNode = findStart(n);
-
-        heights = new int[n];
-        int endNode = getHeight(n, startNode);
-        getHeight(n, endNode);
-        int minHeight = maxHeight / 2 + 1;
-
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            if (heights[i] == minHeight) {
-                answer.add(i);
+            if (numEdges[i] == 1) {
+                q.add(i);
             }
         }
-        return answer;
+
+        while (!q.isEmpty()) {
+            int qn = q.size();
+            remainNode -= qn;
+            if (remainNode == 0) {
+                return new ArrayList<>(q);
+            }
+
+            for (; qn > 0; qn--) {
+                int node = q.poll();
+
+                for (int nNode : tree.get(node)) {
+                    numEdges[nNode]--;
+                    if (numEdges[nNode] == 1) {
+                        q.add(nNode);
+                    }
+                }
+            }
+        }
+        
+
+        return new ArrayList<>(Arrays.asList(0));
     }
 }
