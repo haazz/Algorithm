c494469142337062472628e513cd90b407e30d0b
haazz <127824457+haazz@users.noreply.github.com>
2025-10-01 18:51:28 +0900

    [level 3] Title: 산 모양 타일링, Time: 5.50 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -4,24 +4,21 @@ class Solution {
     public int solution(int n, int[] tops) {
         if (n == 0 || tops == null || tops.length == 0) return 0;
 
-        // 초기값: nonEmpty는 코드상 항상 1로 시작
-        int nonEmpty = 1 % MOD;
-        int answer = (tops[0] == 0 ? 3 : 4) % MOD;
+        int nonEmpty = 1;
+        int answer = tops[0] == 0 ? 3 : 4;
 
         for (int i = 1; i < n; i++) {
-            int prevAnswer = answer;                 // 이미 MOD 범위
+            int prevAnswer = answer;
             if (tops[i] == 0) {
-                // (answer * 3 - nonEmpty) % MOD
-                answer = (int)((answer * 3L) % MOD); // 곱셈 직후 MOD
-                answer = (answer - nonEmpty) % MOD;  // 뺄셈 후 MOD
+                answer = (answer * 3) % MOD;
+                answer = (answer - nonEmpty) % MOD;
             } else {
-                // (answer * 4 - nonEmpty) % MOD
-                answer = (int)((answer * 4L) % MOD);
+                answer = (answer * 4) % MOD;
                 answer = (answer - nonEmpty) % MOD;
             }
-            if (answer < 0) answer += MOD;           // 음수 보정
-            nonEmpty = prevAnswer;                   // 이미 MOD 범위라 그대로 대입
+            if (answer < 0) answer += MOD;
+            nonEmpty = prevAnswer;
         }
-        return answer; // 이미 MOD 범위
+        return answer;
     }
 }
