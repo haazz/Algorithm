140ffed4633acc2e41a83d0f147309ed2932c290
haazz <127824457+haazz@users.noreply.github.com>
2025-10-10 14:08:40 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 63.73 ms, Memory: 209 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -4,45 +4,44 @@ class Solution {
     public int solution(int[][] board, int[][] skill) {
         N = board.length;
         M = board[0].length;
-        int[][] dp = new int[N][M];
+        int[][] prefixSum = new int[N][M];
         
         for (int i = 0; i < skill.length; i++) {
             if (skill[i][0] == 1) {
                 skill[i][5] *= -1;
             }
-            dp[skill[i][1]][skill[i][2]] += skill[i][5];
+            prefixSum[skill[i][1]][skill[i][2]] += skill[i][5];
             if (skill[i][3] + 1 < N) {
-                dp[skill[i][3] + 1][skill[i][2]] += skill[i][5] * -1;
+                prefixSum[skill[i][3] + 1][skill[i][2]] += skill[i][5] * -1;
             }
             if (skill[i][4] + 1 < M) {
-                dp[skill[i][1]][skill[i][4] + 1] += skill[i][5] * -1;
+                prefixSum[skill[i][1]][skill[i][4] + 1] += skill[i][5] * -1;
             }
             if (skill[i][3] + 1 < N && skill[i][4] + 1 < M) {
-                dp[skill[i][3] + 1][skill[i][4] + 1] += skill[i][5];
+                prefixSum[skill[i][3] + 1][skill[i][4] + 1] += skill[i][5];
             }
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 1; j < M; j++) {
-                dp[i][j] += dp[i][j - 1];
+                prefixSum[i][j] += prefixSum[i][j - 1];
             }
         }
         
         int answer = 0;
         for (int j = 0; j < M; j++) {
             for (int i = 1; i < N; i++) {
-                dp[i][j] += dp[i - 1][j];
+                prefixSum[i][j] += prefixSum[i - 1][j];
                   
             }
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (board[i][j] + dp[i][j] >= 1) {
+                if (board[i][j] + prefixSum[i][j] >= 1) {
                     answer++;
                 }
             }
-            System.out.println();
         }
         return answer;
     }
