384ab0a4c9d00acbfff5396c3a4debe618727943
haazz <127824457+haazz@users.noreply.github.com>
2025-10-10 21:08:39 +0900

    [Gold III] Title: 소문난 칠공주, Time: 440 ms, Memory: 152832 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,128 +1,80 @@
-// 4명의 S를 반드시 갖고 있어야 한다.
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-/**
- * 1. 7개가 될 수 있는 조합 구하기
- * 2. 연결된 조합인지 확인
- * 3. + 이다솜파 (S) 4개가 포함 되는지 체크
- */
-class Main {
+public class Main {
     static final int N = 5;
-    static final int[] dy = { 0, -1, 0, 1 };
-    static final int[] dx = { -1, 0, 1, 0 };
-    static int result;
+    static int[] dy = { 0, 1, 0, -1 };
+    static int[] dx = { 1, 0, -1, 0 };
+    static int answer = 0;
 
-    public static boolean isMoreFourY(char[][] board, boolean[][] visit) {
-        // 'Y'가 4개 이상인지 체크
-        int cnt = 0;
+    public static boolean can(char[][] board, boolean[][] visit, int y, int x) {
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visit[i][j] && board[i][j] == 'S') {
-                    cnt++;
-                }
-            }
-        }
-
-        if (cnt >= 4) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean bfs(boolean[][] visit, int ci, int cj) {
-        int cnt = 0;
-        boolean[][] bfsVisit = new boolean[N][N];
+        int s = 0;
+        int con = 0;
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { ci, cj });
+        boolean[][] bVisit = new boolean[N][N];
+        q.add(new int[] { y, x - 1 });
 
         while (!q.isEmpty()) {
             int[] elem = q.poll();
-            bfsVisit[elem[0]][elem[1]] = true;
-            cnt++;
 
             for (int d = 0; d < 4; d++) {
                 int ny = dy[d] + elem[0];
                 int nx = dx[d] + elem[1];
 
-                if (ny < 0 || ny >= N || nx < 0 || nx >= N
-                        || bfsVisit[ny][nx] || !visit[ny][nx]) {
+                if (ny < 0 || ny >= N || nx < 0 || nx >= N || bVisit[ny][nx] || !visit[ny][nx]) {
                     continue;
+                }
+
+                if (board[ny][nx] == 'S') {
+                    s++;
                 }
                 q.add(new int[] { ny, nx });
-                bfsVisit[ny][nx] = true;
+                bVisit[ny][nx] = true;
+                con++;
             }
         }
 
-        if (cnt == 7) {
-            return true;
+        if (s < 4 || con < 7) {
+            return false;
         }
-        return false;
+        return true;
     }
 
-    public static boolean isConnect(char[][] board, boolean[][] visit) {
-        // bfs를 이용한 연결 체크
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visit[i][j]) {
-                    continue;
-                }
-                return bfs(visit, i, j);
-            }
-        }
-        return false;
-    }
-
-    public static void dfs(char[][] board, boolean[][] visit, int start, int depth) {
+    public static void comb(char[][] board, boolean[][] visit, int y, int x, int depth) {
+        // 연결될 수 있는지 체크 + S가 4개 이상인지 체크
         if (depth >= 7) {
-            if (isMoreFourY(board, visit) && isConnect(board, visit)) {
-                result++;
+            if (can(board, visit, y, x)) {
+                answer++;
             }
             return;
         }
 
-        // 1. 조합 구하기
-        for (int i = start / 5; i < N; i++) {
+        for (int i = y; i < N; i++) {
             int j = 0;
-            if (i == start / 5) {
-                j = start % 5;
+            if (i == y) {
+                j = x;
             }
             for (; j < N; j++) {
-                if (visit[i][j]) {
-                    continue;
-                }
                 visit[i][j] = true;
-                dfs(board, visit, (i * 5) + j + 1, depth + 1);
+                comb(board, visit, i, j + 1, depth + 1);
                 visit[i][j] = false;
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         char[][] board = new char[N][N];
-        List<int[]> lees = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split("");
+            String s = br.readLine();
             for (int j = 0; j < N; j++) {
-                board[i][j] = s[j].charAt(0);
-                if (board[i][j] == 'S') {
-                    lees.add(new int[] { i, j });
-                }
+                board[i][j] = s.charAt(j);
             }
         }
-
-        dfs(board, new boolean[N][N], 0, 0);
-        System.out.println(result);
-
+        comb(board, new boolean[N][N], 0, 0, 0);
+        System.out.println(answer);
     }
 }
