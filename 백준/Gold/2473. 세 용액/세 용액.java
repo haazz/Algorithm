0cd2dc396cfcf908cdcd32cc7fc8cb27c9a93b05
haazz <127824457+haazz@users.noreply.github.com>
2025-10-10 20:27:25 +0900

    [Gold III] Title: 세 용액, Time: 616 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,56 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Main {
-    static int N;
-    static final long MAX_VALUE = Integer.MAX_VALUE;
+public class Main {
+    public static int bs(long[] req, long target, int l, int r) {
+        int answer = r;
+        long minsub = Long.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            long sub = Math.abs(req[mid] - target);
+            if (minsub > sub) {
+                minsub = sub;
+                answer = mid;
+            }
+            if (req[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        long[] liquid = new long[N];
+        int N = Integer.parseInt(br.readLine());
+        long[] req = new long[N];
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            liquid[i] = Long.parseLong(st.nextToken());
+            req[i] = Long.parseLong(st.nextToken());
         }
-        Arrays.sort(liquid);
 
-        long[] result = new long[3];
-        result[0] = MAX_VALUE;
-        result[1] = MAX_VALUE;
-        result[2] = MAX_VALUE;
-
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                int start = j + 1;
-                int end = N - 1;
-
-                while (start <= end) {
-                    int mid = (start + end) / 2;
-
-                    if (Math.abs(liquid[i] + liquid[j] + liquid[mid]) < Math
-                            .abs(result[0] + result[1] + result[2])) {
-                        result[0] = liquid[i];
-                        result[1] = liquid[j];
-                        result[2] = liquid[mid];
-                    }
-                    
-                    if (liquid[i] + liquid[j] + liquid[mid] > 0) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
+        Arrays.sort(req);
+        long[] answer = new long[3];
+        long minsub = Long.MAX_VALUE;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                int k = bs(req, (req[i] + req[j]) * -1, j + 1, N - 1);
+                long sum = Math.abs(req[i] + req[j] + req[k]);
+                if (minsub > sum) {
+                    minsub = sum;
+                    answer[0] = req[i];
+                    answer[1] = req[j];
+                    answer[2] = req[k];
                 }
             }
         }
-
-        Arrays.sort(result);
-
-        System.out.println(result[0] + " " + result[1] + " " + result[2]);
-
+        for (int i = 0; i < 3; i++) {
+            System.out.print(answer[i] + " ");
+        }
+        System.out.println();
     }
 }
