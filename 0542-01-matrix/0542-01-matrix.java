7d8f5e549176561ec1b7d517b84b9c4b35c238ee
haazz <127824457+haazz@users.noreply.github.com>
2025-12-18 15:58:31 +0900

    Time: 20 ms (9.63%), Space: 49.2 MB (33.32%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] dy = {0, 1, 0, -1};
+        int[] dx = {1, 0, -1, 0};
+        
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new int[] {i, j, 0});
+                } else {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] elem = q.poll();
+            
+            for (int d = 0; d < 4; d++) {
+                int ny = dy[d] + elem[0];
+                int nx = dx[d] + elem[1];
+
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m || mat[ny][nx] == 0 || mat[ny][nx] <= elem[2]) {
+                    continue;
+                }
+                mat[ny][nx] = elem[2] + 1;
+                q.add(new int[] {ny, nx, elem[2] + 1});
+            }
+            
+        }
+
+        return mat;
+    }
+}
