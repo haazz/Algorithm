c9838d78af4af268fb1bcc4568a2321a06ab5cb9
haazz <127824457+haazz@users.noreply.github.com>
2026-01-08 12:19:48 +0900

    Time: 3 ms (77.14%), Space: 45.7 MB (74.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,20 +1,6 @@
 class Solution {
-    class Node {
-        int idx;
-        int val;
-        Node next;
-        Node prev;
-
-        public Node(int idx, int val, Node prev) {
-            this.idx = idx;
-            this.val = val;
-            this.next = null;
-            this.prev = prev;
-        }
-    }
     public String minWindow(String s, String t) {
         int[] alp = new int[52];
-        HashMap<Integer, Queue<Node>> hm = new HashMap<>();
 
         for (int i = 0; i < t.length(); i++) {
             int val;
@@ -25,55 +11,33 @@ class Solution {
                 val = t.charAt(i) - 'A' + 26;
                 alp[val]++;
             }
-            hm.put(val, new LinkedList<>());
         }
 
-        
-        Node root = new Node(0, 0, null);
-        Node end = new Node(0, 0, root);
-        root.next = end;
-
+        int cnt = t.length();
+        int msi = 0;
+        int start = 0;
         int minSize = Integer.MAX_VALUE;
-        int minStartIdx = 0;
-        int qSize = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             int val = ch >= 'a' && ch <= 'z' ? ch - 'a' : ch - 'A' + 26;
-            Queue<Node> q = hm.get(val);
 
-            if (q == null) {
-                continue;
-            }
-            
-
-            if (q.size() < alp[val]) {
-                Node nNode = new Node(i, val, end.prev);
-                q.add(nNode);
-                end.prev.next = nNode;
-                end.prev = nNode;
-                qSize++;
-            } else {
-                Node nNode = new Node(i, val, end.prev);
-                Node pNode = q.poll();
-                q.add(nNode);
-                end.prev.next = nNode;
-                end.prev = nNode;
-                pNode.next.prev = pNode.prev;
-                pNode.prev.next = pNode.next;
-                
+            if (--alp[val] >= 0) {
+                cnt--;
             }
 
-            if (qSize >= t.length()) {
-                int l = root.next.idx;
-                int r = end.prev.idx;
-
-                if (r - l < minSize) {
-                    minStartIdx = l;
-                    minSize = r - l;
+            while (cnt <= 0) {
+                int sVal = s.charAt(start) >= 'a' && s.charAt(start) <= 'z' ? s.charAt(start) - 'a' : s.charAt(start) - 'A' + 26;
+                if (++alp[sVal] > 0) {
+                    cnt++;
+                    if (minSize > i - start) {
+                        msi = start;
+                        minSize = i - start;
+                    }
                 }
+                start++;
             }
         }
-        return minSize == Integer.MAX_VALUE ? "" : s.substring(minStartIdx, minStartIdx + minSize + 1);
+        return minSize == Integer.MAX_VALUE ? "" : s.substring(msi, msi + minSize + 1);
     }
 }
