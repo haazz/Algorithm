df30067d5e1b9979528d7aeb9d2fb81a98164a7b
haazz <127824457+haazz@users.noreply.github.com>
2026-04-02 21:00:40 +0900

    Time: 19 ms (42.23%), Space: 47.8 MB (27.42%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,49 +1,52 @@
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> win = new HashMap<Character, Integer>();
         
         for (int i  = 0; i < t.length(); i++) {
             char ch = t.charAt(i);
             hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
         
+        int target = hm.size();
         int cnt = 0;
-        int mLen = Integer.MAX_VALUE;
-        int mli = 0;
-        int mri = 0;
         int li = 0;
+        int rli = 0;
+        int rri = Integer.MAX_VALUE;
 
         for (int ri = 0; ri < s.length(); ri++) {
             char ch = s.charAt(ri);
-            if (hm.containsKey(ch)) {
-                // general proccess
-                hm.put(ch, hm.get(ch) - 1);
-                if (hm.get(ch) >= 0 && ++cnt >= t.length() && mLen > ri - li) {
-                    mLen = ri - li;
-                    mli = li;
-                    mri = ri;
-                }
-                
-                // minus proccess
-                char lch = s.charAt(li);
-                while (!hm.containsKey(lch) || hm.get(lch) < 0) {
-                    if (hm.containsKey(lch)) {
-                        hm.put(lch, hm.get(lch) + 1);
-                    }
-                    li++;
-                    lch = s.charAt(li);
-                }
-                if (cnt >= t.length() && mLen > ri - li) {
-                    mLen = ri - li;
-                    mli = li;
-                    mri = ri;
-                }
-                
+
+            win.put(ch, win.getOrDefault(ch, 0) + 1);
+
+            if (hm.containsKey(ch) && hm.get(ch).equals(win.get(ch))) {
+                cnt++;
+
             }
+
+            while (cnt == target) {
+                
+                if (rri - rli > ri - li) {
+                    rli = li;
+                    rri = ri;
+                }
+
+                char lch = s.charAt(li);
+                
+                win.put(lch, win.get(lch) - 1);
+
+                if (hm.containsKey(lch) && hm.get(lch) > win.get(lch)) {    
+                    cnt--;
+                }
+                li++;
+            }
+
         }
-        if (cnt < t.length()) {
-            return  "";
+
+
+        if (rri == Integer.MAX_VALUE) {
+            return "";
         }
-        return s.substring(mli, mri + 1);
+        return s.substring(rli, rri + 1);
     }
 }
