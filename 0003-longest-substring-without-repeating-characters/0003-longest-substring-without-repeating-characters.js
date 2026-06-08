bfd822ae888f1a916fdca05e3e79e5267e689c26
haazz <127824457+haazz@users.noreply.github.com>
2026-06-08 12:08:22 +0900

    Time: 19 ms (23.52%), Space: 60.6 MB (17.03%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const map = {};
+
+    let l = -1;
+    let max = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const ch = s.charAt(i);
+    
+        if (map[ch] != null) {
+            l = Math.max(map[ch], l);
+        }
+        map[ch] = i;
+        max = Math.max(max, i - l);
+    }
+    return max;
+};
