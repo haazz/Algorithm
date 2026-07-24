b7f36d7a9aa819672cbbd08fa8042d0299798070
haazz <127824457+haazz@users.noreply.github.com>
2026-07-24 20:39:08 +0900

    Time: 363 ms (5.14%), Space: 47.8 MB (5.2%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,72 @@
+class Solution {
+    int N;
+    int M;
+    int[][] dp;
+    int[] dy = {0, 1, 0, -1};
+    int[] dx = {1, 0, -1, 0};
+
+    public void bfs(int[][] matrix, int sy, int sx) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {sy, sx, 1});
+        dp[sy][sx] = 1;
+
+        while (!q.isEmpty()) {
+            int[] elem = q.poll();
+            
+            for (int d = 0; d < 4; d++) {
+                int ny = elem[0] + dy[d];
+                int nx = elem[1] + dx[d];
+                
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M || 
+                    matrix[ny][nx] <= matrix[elem[0]][elem[1]] || 
+                    dp[ny][nx] >= elem[2] + 1) {
+                    continue;
+                }
+                dp[ny][nx] = elem[2] + 1;
+                q.add(new int[] {ny, nx, elem[2] + 1});
+            }
+        }
+
+    }
+
+    public int longestIncreasingPath(int[][] matrix) {
+        N = matrix.length;
+        M = matrix[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        dp = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                pq.add(new int[] {i, j, matrix[i][j]});
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            int[] elem = pq.poll();
+            if (dp[elem[0]][elem[1]] >= 1) {
+                continue;
+            }
+            bfs(matrix, elem[0], elem[1]);
+        //     System.out.println(matrix[elem[0]][elem[1]]);
+        //     for (int i = 0; i < N; i++) {
+        //     for (int j = 0; j < M; j++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        // System.out.println("");
+
+        }
+
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                // System.out.print(dp[i][j] + " ");
+                res = Math.max(res, dp[i][j]);
+            }
+            // System.out.println("");
+        }
+        // System.out.println("");
+
+        return res;
+    }
+}
